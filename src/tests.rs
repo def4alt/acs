f80dab3a118aa2258dd7d09d6ae3abf5888abe1d
Andrii Olkhovych <the.def4alt@gmail.com>
2023-02-20 00:38:36 +0100

    Abstract out operations to use generic types

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,7 @@
+use std::ops::{Add, Div, Mul, Sub};
+
+use num_traits::{one, Bounded, Num};
+
 macro_rules! x10 {
     ($function:ident) => {
         std::hint::black_box($function());
@@ -69,156 +73,34 @@ where
     x10000!(func);
 }
 
-const NUM_I128: i128 = std::i128::MAX / 8;
-const NUM_I64: i64 = std::i64::MAX / 9;
-const NUM_I32: i32 = std::i32::MAX / 7;
-const NUM_I16: i16 = std::i16::MAX / 3;
-const NUM_I8: i8 = std::i8::MAX / 5;
-const NUM_F64: f64 = std::f64::MAX / 654.43563;
-const NUM_F32: f32 = std::f32::MAX / 764.876867;
-
-pub fn add_i128() {
+pub fn add<T: Num + Add<Output = T> + Copy + Div<Output = T> + Bounded>() {
+    let max = <T>::max_value() / (one::<T>() + one::<T>() + one::<T>() + one::<T>() + one::<T>());
+    let value = one::<T>() + one::<T>() + one::<T>() + one::<T>() + one::<T>();
     body(|| {
-        std::hint::black_box(NUM_I128 + 40);
+        std::hint::black_box(max + value);
     })
 }
 
-pub fn add_i64() {
+pub fn substract<T: Num + Add<Output = T> + Copy + Div<Output = T> + Sub<Output = T> + Bounded>() {
+    let max = <T>::max_value();
+    let value = one::<T>() + one::<T>() + one::<T>() + one::<T>() + one::<T>();
     body(|| {
-        std::hint::black_box(NUM_I64 + 76);
+        std::hint::black_box(max - value);
     })
 }
 
-pub fn add_i32() {
+pub fn divide<T: Num + Add<Output = T> + Copy + Div<Output = T> + Bounded>() {
+    let max = <T>::max_value();
+    let value = one::<T>() + one::<T>() + one::<T>() + one::<T>() + one::<T>();
     body(|| {
-        std::hint::black_box(NUM_I32 + 65);
-    })
-}
-pub fn add_i16() {
-    body(|| {
-        std::hint::black_box(NUM_I16 + 3);
-    })
-}
-pub fn add_i8() {
-    body(|| {
-        std::hint::black_box(NUM_I8 + 3);
-    })
-}
-pub fn add_f64() {
-    body(|| {
-        std::hint::black_box(NUM_F64 + 75.234);
-    })
-}
-pub fn add_f32() {
-    body(|| {
-        std::hint::black_box(NUM_F32 + 243.565);
-    })
-}
-pub fn substract_i128() {
-    body(|| {
-        std::hint::black_box(std::i128::MAX - 234235);
-    })
-}
-pub fn substract_i64() {
-    body(|| {
-        std::hint::black_box(std::i64::MAX - 234235);
-    })
-}
-pub fn substract_i32() {
-    body(|| {
-        std::hint::black_box(std::i32::MAX - 24234);
-    })
-}
-pub fn substract_i16() {
-    body(|| {
-        std::hint::black_box(std::i16::MAX - 23423);
-    })
-}
-pub fn substract_i8() {
-    body(|| {
-        std::hint::black_box(std::i8::MAX - 23);
-    })
-}
-pub fn substract_f64() {
-    body(|| {
-        std::hint::black_box(std::f64::MAX - 53254.7657);
-    })
-}
-pub fn substract_f32() {
-    body(|| {
-        std::hint::black_box(std::f32::MAX - 3236.543);
+        std::hint::black_box(max / value);
     })
 }
 
-pub fn divide_i128() {
+pub fn multiply<T: Num + Add<Output = T> + Copy + Div<Output = T> + Mul<Output = T> + Bounded>() {
+    let max = <T>::max_value() / (one::<T>() + one::<T>() + one::<T>() + one::<T>() + one::<T>());
+    let value = one::<T>() + one::<T>();
     body(|| {
-        std::hint::black_box(std::i128::MAX / 1245);
-    })
-}
-pub fn divide_i64() {
-    body(|| {
-        std::hint::black_box(std::i64::MAX / 6545);
-    })
-}
-pub fn divide_i32() {
-    body(|| {
-        std::hint::black_box(std::i32::MAX / 4234);
-    })
-}
-pub fn divide_i16() {
-    body(|| {
-        std::hint::black_box(std::i16::MAX / 346);
-    })
-}
-pub fn divide_i8() {
-    body(|| {
-        std::hint::black_box(std::i8::MAX / 12);
-    })
-}
-pub fn divide_f64() {
-    body(|| {
-        std::hint::black_box(std::f64::MAX / 165.643);
-    })
-}
-pub fn divide_f32() {
-    body(|| {
-        std::hint::black_box(std::f32::MAX / 347.1235);
-    })
-}
-
-pub fn multiply_i128() {
-    body(|| {
-        std::hint::black_box(NUM_I128 * 6);
-    })
-}
-
-pub fn multiply_i64() {
-    body(|| {
-        std::hint::black_box(NUM_I64 * 2);
-    })
-}
-pub fn multiply_i32() {
-    body(|| {
-        std::hint::black_box(NUM_I32 * 3);
-    })
-}
-pub fn multiply_i16() {
-    body(|| {
-        std::hint::black_box(NUM_I16 * 2);
-    })
-}
-pub fn multiply_i8() {
-    body(|| {
-        std::hint::black_box(NUM_I8 * 2);
-    })
-}
-pub fn multiply_f64() {
-    body(|| {
-        std::hint::black_box(NUM_F64 * 7.234f64);
-    })
-}
-pub fn multiply_f32() {
-    body(|| {
-        std::hint::black_box(NUM_F32 * 5.234f32);
+        std::hint::black_box(max * value);
     })
 }
